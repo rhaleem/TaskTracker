44b6939c16933b9619929eaa888c69d749a336bb
rhaleem <reza.haleem@gmail.com>
2021-01-29 21:59:25 -0800

    add task add toggle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,26 +26,14 @@ const App = () => {
     },
   ]);
 
-  // const [tasks, setTasks] = useState([
-  //   {
-  //     id: 1,
-  //     text: 'Doctors Appointment',
-  //     day: 'Feb 5th at 2:30pm',
-  //     reminder: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     text: 'Meeting at School',
-  //     day: 'Feb 6th at 1:30pm',
-  //     reminder: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     text: 'Food Shopping',
-  //     day: 'Feb 5th at 2:30pm',
-  //     reminder: false,
-  //   },
-  // ]);
+  // Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+
+    setTasks([...tasks, newTask]);
+  };
 
   // Delete Tasks
   const deleteTask = (id) => {
@@ -52,17 +42,24 @@ const App = () => {
 
   // Toggle Reminder
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => (task.id === id ? {...task, reminder: !task.reminder} : task)));
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className='container'>
-      <Header title='Task Tracker' />
-      {tasks.length > 0 ? (
+      <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
+
+      {tasks.length >= 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         'No Tasks To Show'
       )}
+      {/* <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> */}
     </div>
   );
 };
